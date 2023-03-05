786c33522014576c80c7b155084541fe353413cd
Mike Kühnapfel <mike.kuehnapfel@piraten-rek.de>
2023-03-05 19:11:27 +0100

    add session testing code to index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,20 @@
 import type { NextPage } from 'next'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 const Home: NextPage = () => {
+  const { data: session } = useSession()
+
+  console.log(session)
+
   return (
-    <div><p>Hello World</p></div>
+    <>
+      <div><p>Hello World</p></div>
+      {
+        session?.user != null
+          ? <button onClick={() => { void signOut() }}>Logout</button>
+          : <button onClick={() => { void signIn() }}>Login</button>
+      }
+    </>
   )
 }
 
