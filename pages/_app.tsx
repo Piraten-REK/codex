f61fb82f1a41a477eecf248b5f55feeaa8759fcf
Mike Kühnapfel <mike.kuehnapfel@piraten-rek.de>
2023-03-06 20:38:19 +0100

    add Inter font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,27 @@
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
+import { Inter } from 'next/font/google'
+import '../styles/globals.css'
+
+const inter = Inter({ subsets: ['latin', 'latin-ext'] })
 
 const Codex = ({
   Component,
   pageProps: { session, ...pageProps }
 }: AppProps): JSX.Element => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <style jsx global>
+        {`
+          :root {
+            font-family: ${inter.style.fontFamily};
+          }
+        `}
+      </style>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   )
 }
 
