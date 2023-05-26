0ac290d55ccde141229892abeab900847c47bfa7
Mike Kühnapfel <mike.kuehnapfel@piraten-rek.de>
2023-05-27 00:38:23 +0200

    add support for tabbed pages to Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,15 +1,38 @@
-import type { FC, PropsWithChildren } from 'react'
+import type { FC, PropsWithChildren, ReactNode } from 'react'
 import Head from 'next/head'
 import Sidebar from './Sidebar'
 import Header from './Header'
+import { Menu, Tab, TabPanel, Tabs } from './Tabs'
 
 export interface LayoutProps {
   title?: string
   description?: string
   className?: string
+  tabs?: Array<{
+    key: string
+    title: string
+    content: ReactNode
+  }>
 }
 
-const Layout: FC<PropsWithChildren<LayoutProps>> = ({ title, description, className, children }) => {
+const Layout: FC<PropsWithChildren<LayoutProps>> = ({ title, description, className, tabs, children }) => {
+  const content = tabs != null && tabs.length > 0
+    ? (
+      <Tabs defaultActiveKey={tabs[0].key}>
+        <Menu>
+          {tabs.map(({ key, title }) => <Tab key={key} eventKey={key}>{title}</Tab>)}
+        </Menu>
+        <main className={className}>
+          {tabs.map(({ key, content }) => <TabPanel key={key} eventKey={key}>{content}</TabPanel>)}
+        </main>
+      </Tabs>
+      )
+    : (
+      <main className={className}>
+        {children}
+      </main>
+      )
+
   return (
     <>
       <Head>
@@ -21,9 +44,7 @@ const Layout: FC<PropsWithChildren<LayoutProps>> = ({ title, description, classN
       </Head>
       <Header title={title} />
       <Sidebar />
-      <main className={className}>
-        {children}
-      </main>
+      {content}
     </>
   )
 }
