71d17c1f41fb66d2796ffe0fafc6307e9a0bf93b
Mike Kühnapfel <mike.kuehnapfel@piraten-rek.de>
2023-05-26 20:08:17 +0200

    add useClassName hook

--- utils/hooks.ts
+++ utils/hooks.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import type { Dispatch, SetStateAction } from 'react'
 
 export const useToggle = (initialValue: boolean): [boolean, () => void, Dispatch<SetStateAction<boolean>>] => {
@@ -6,4 +6,11 @@ export const useToggle = (initialValue: boolean): [boolean, () => void, Dispatch
   const toggleValue = (): void => setValue(prev => !prev)
 
   return [value, toggleValue, setValue]
+}
+
+export const useClassName = (stylesClass: string, ...classes: Array<string | undefined | false>): string => {
+  return useMemo(
+    () => [stylesClass].concat(classes.filter(Boolean)).join(' '),
+    [classes]
+  )
 }
