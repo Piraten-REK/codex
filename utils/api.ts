db2ea65a0a4af99ec779bec2896bdeb57e13be8a
Mike Kühnapfel <mike.kuehnapfel@piraten-rek.de>
2023-06-23 21:45:05 +0200

    add repond helper function

--- utils/api.ts
+++ utils/api.ts
@@ -1,10 +1,14 @@
-import { File } from '@prisma/client'
-import fs from 'fs'
-import path from 'path'
+import { NextApiResponse } from 'next'
 
-export const getFile = async (fileId: File['id']): Promise<Buffer | null> => await new Promise<Buffer | null>((resolve, reject) => {
-  fs.readFile(path.resolve(process.cwd(), `./data/${fileId}`), (err, data) => {
-    if (err != null) reject(err)
-    else resolve(data)
-  })
-})
+export const respond = (res: NextApiResponse, status: number, data: unknown, meta?: { [key: string]: unknown }): void => {
+  const json = {
+    success: status < 400,
+    [status < 400 ? 'data' : 'error']: data
+  }
+
+  if (meta != null) json.meta = meta
+
+  res
+    .status(status)
+    .json(json)
+}
